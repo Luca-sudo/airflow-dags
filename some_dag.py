de53fe34d88c73c141fcac438002a7c2e2e39276
Luca Witt <witt-luca@web.de>
2025-07-30 15:43:44 +0200

    Postgres Operator

--- some_dag.py
+++ some_dag.py
@@ -2,6 +2,7 @@ import datetime
 
 from airflow.sdk import DAG
 from airflow.providers.standard.operators.empty import EmptyOperator
+from airflow.providers.postgres.operators.postgres import PostgresOperator
 
 with DAG(
     dag_id="my_dag_name",
@@ -12,6 +13,24 @@ with DAG(
     op1 = EmptyOperator(task_id="task1")
     op2 = EmptyOperator(task_id="task2")
     op3 = EmptyOperator(task_id="task3")
-    op4 = EmptyOperator(task_id="task4")
+    op4 = PostgresOperator(
+            task_id="create_table",
+            postgres_conn_id="postgres",
+            sql="""
+            CREATE TABLE IF NOT EXISTS some_table(
+                id SERIAL PRIMARY KEY,
+                name VARCHAR(100) NOT NULL,
+                created_at TIMESTAMP DEFAULT NOW()
+            );
+            """
+            )
+    op5 = PostgresOperator(
+            task_id="insert_names",
+            postgres_conn_id="postgres",
+            sql="""
+            INSERT INTO some_table (name) VALUES ('reinhard');
+            """
+            )
     op1 >> op3 >> op4 >> op2
     op1 >> op2
+    op4 >> op5
